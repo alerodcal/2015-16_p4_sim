74d7844fbd0394607bbf4d3875d389883decde85
Alejandro Rodríguez Calzado <alerguez94@gmail.com>
2015-11-05 18:35:48 +0100

    Añadida funcionalidad de error de bit. Parece que funciona...

--- simulador.cc
+++ simulador.cc
@@ -4,6 +4,9 @@
 #include <ns3/point-to-point-helper.h>
 #include <ns3/data-rate.h>
 #include <ns3/error-model.h>
+#include <ns3/random-variable-stream.h>
+#include <ns3/error-model.h>
+#include <ns3/point-to-point-net-device.h>
 #include "Enlace.h"
 #include "Observador.h"
 
@@ -23,6 +26,15 @@ main (int argc, char *argv[])
   Time     rprop      = Time("4ms");
   DataRate vtx        = DataRate("1000kbps");
   uint8_t  tamVentana = 3;
+  double   perror     = 0.00002;
+
+  // Creamos el modelo de error y le asociamos los parametros
+  Ptr<UniformRandomVariable> uniforme = CreateObject<UniformRandomVariable>();
+  Ptr<RateErrorModel> modelo_error = CreateObject<RateErrorModel> ();
+
+  modelo_error->SetRandomVariable(uniforme);
+  modelo_error->SetRate(perror);
+  modelo_error->SetUnit(RateErrorModel::ERROR_UNIT_BIT);
 
   // Configuramos el escenario:
   PointToPointHelper escenario;
@@ -35,15 +47,19 @@ main (int argc, char *argv[])
   // Creamos el escenario
   NetDeviceContainer dispositivos = escenario.Install (nodos);
 
-  Observador observador;
-  // Suscribimos la traza de paquetes correctamente asentidos.
-  dispositivos.Get (0)->TraceConnectWithoutContext ("MacRx", MakeCallback(&Observador::PaqueteAsentido, &observador));
-  dispositivos.Get (0)->TraceConnectWithoutContext ("PhyRxDrop", MakeCallback(&Observador::PaqueteErroneo, &observador));
-
   // Una aplicación transmisora (que tambien recibira)
   Enlace transmisor (dispositivos.Get (1), trtx, tamPaquete, tamVentana);
   // Y una receptora (que tambien enviara)
   Enlace receptor (dispositivos.Get (0), trtx, tamPaquete, tamVentana);
+
+  dispositivos.Get(0)->GetObject<PointToPointNetDevice>()->SetReceiveErrorModel(modelo_error);
+  dispositivos.Get(1)->GetObject<PointToPointNetDevice>()->SetReceiveErrorModel(modelo_error);
+
+  Observador observador;
+  // Suscribimos la traza de paquetes correctamente asentidos.
+  dispositivos.Get (0)->TraceConnectWithoutContext ("MacRx", MakeCallback(&Observador::PaqueteAsentido, &observador));
+  dispositivos.Get (1)->GetObject<PointToPointNetDevice>()->TraceConnectWithoutContext ("PhyRxDrop", MakeCallback(&Observador::PaqueteErroneo, &observador));
+
   // Añadimos cada aplicación a su nodo
   nodos.Get (0)->AddApplication(&transmisor);
   nodos.Get (1)->AddApplication(&receptor);
@@ -66,7 +82,8 @@ main (int argc, char *argv[])
   NS_LOG_DEBUG ("Rprop: " << rprop);
   NS_LOG_DEBUG ("RTT: " << Seconds(vtx.CalculateTxTime (tamPaquete + 8)) + 2 * rprop);
   NS_LOG_DEBUG ("Temporizador: " << trtx);
+  NS_LOG_DEBUG ("Probabilidad de error de bit: " << perror);
   NS_LOG_INFO  ("Total paquetes: " << observador.TotalPaquetes ());
-  
+  NS_LOG_INFO  ("Total paquetes erroneos: " << observador.TotalPaquetesErroneos ());
   return 0;
 }
